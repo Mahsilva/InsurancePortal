1da4c3391e04fb5cfe4a8bdb51f50eab7de623b1
Marcelo <mahsilva001@gmail.com>
2026-03-26 21:07:14 +1100

    auto migrate on startup

--- DatabaseInsurance/Program.cs
+++ DatabaseInsurance/Program.cs
@@ -22,6 +22,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseCors();
 app.UseSwagger();
 app.UseSwaggerUI();

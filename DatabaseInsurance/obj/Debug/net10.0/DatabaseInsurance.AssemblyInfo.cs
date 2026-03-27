d57a0849c5a5ddf911085a374e5e034e663d8eab
Marcelo <mahsilva001@gmail.com>
2026-03-27 18:47:44 +1100

    fix migrations and dockerfile

--- DatabaseInsurance/obj/Debug/net10.0/DatabaseInsurance.AssemblyInfo.cs
+++ DatabaseInsurance/obj/Debug/net10.0/DatabaseInsurance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da4c3391e04fb5cfe4a8bdb51f50eab7de623b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fb1f61f331cb56fca5c29346a77db848bff890")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

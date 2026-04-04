c8ba33ce83995f486b52a4a90dc5738183ff0b74
Marcelo <mahsilva001@gmail.com>
2026-04-04 20:33:37 +1100

    fix10

--- InsurancePortal/obj/Debug/net10.0/InsurancePortal.AssemblyInfo.cs
+++ InsurancePortal/obj/Debug/net10.0/InsurancePortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba396c0318003314f69c49a9ea799373bf91af0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9525b79d92a1a60c004dab391308c9c99174345")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

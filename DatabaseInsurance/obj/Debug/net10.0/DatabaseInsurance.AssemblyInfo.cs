731e11bd8653c362e2a6e89e434332c88d69dcbe
Marcelo <mahsilva001@gmail.com>
2026-04-10 19:45:41 +1000

    fix10-04

--- DatabaseInsurance/obj/Debug/net10.0/DatabaseInsurance.AssemblyInfo.cs
+++ DatabaseInsurance/obj/Debug/net10.0/DatabaseInsurance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10c981e9a20f303825479468fd8ff41867f6f80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9633f018aac69d083117ab2da90ebf46d1135c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

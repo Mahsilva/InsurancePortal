01af877a9add40a745eb8a15ba7f704205da1c67
Marcelo <mahsilva001@gmail.com>
2026-05-10 18:29:55 +1000

    fix legacy timestamp

--- InsurancePortal/obj/Debug/net10.0/InsurancePortal.AssemblyInfo.cs
+++ InsurancePortal/obj/Debug/net10.0/InsurancePortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627cc9b2ef49a40c280a6b3203466ce1720e08ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd60602f8a697fcaa59bb56641b8aa70b3171e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

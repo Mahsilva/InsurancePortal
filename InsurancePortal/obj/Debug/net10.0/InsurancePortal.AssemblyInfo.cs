7199659b13a8af66be5f9cfc2e9b48a5e657032f
Marcelo <mahsilva001@gmail.com>
2026-05-11 14:25:07 +1000

    fix claim route conflict

--- InsurancePortal/obj/Debug/net10.0/InsurancePortal.AssemblyInfo.cs
+++ InsurancePortal/obj/Debug/net10.0/InsurancePortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75205ce6fcda90b1aaa302da80fce0484ebfc0f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c1ead9d99dc1425ee188c3a18dcd0029cbc708")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsurancePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

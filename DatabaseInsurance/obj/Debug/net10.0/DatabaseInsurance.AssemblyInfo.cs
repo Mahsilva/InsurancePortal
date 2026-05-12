5da7172eb370c007f9031c34abefd3d4e80b8339
Marcelo <mahsilva001@gmail.com>
2026-05-12 17:03:34 +1000

    fixing claim connections

--- DatabaseInsurance/obj/Debug/net10.0/DatabaseInsurance.AssemblyInfo.cs
+++ DatabaseInsurance/obj/Debug/net10.0/DatabaseInsurance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0339116aa4fb11b42fd7eda3aba8138884f21bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed99933ced9ee8139b2b37791387c50a14fa6722")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseInsurance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
